5531f1022247600d9fa150a6ccd563cdcfff38bb
blueyMasterMods <roberttkahelin8@users.noreply.github.com>
2023-07-21 23:33:30 -0400

    Update ThingamajigsCreativeTab.java
    updated to 1.6.3 tabs version

--- rawsrc/1.20/item/ThingamajigsCreativeTab.java
+++ rawsrc/1.20/item/ThingamajigsCreativeTab.java
@@ -3,28 +3,37 @@ package net.rk.thingamajigs.item;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import net.rk.thingamajigs.Thingamajigs;
+import net.rk.thingamajigs.block.ThingamajigsBlocks;
 
 @Mod.EventBusSubscriber(modid = Thingamajigs.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ThingamajigsCreativeTab {
     public static final DeferredRegister<CreativeModeTab> CMTS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Thingamajigs.MOD_ID);
 
-    @Deprecated // since when? mod ver 1.6.2
-    public static CreativeModeTab ALL_ITEMS_TAB; // legacy stuff has ended, so this will go unused indefinitely
+    // deprecated since mod ver 1.6.2-1.6.3+
+    // public static CreativeModeTab ALL_ITEMS_TAB;
 
-    // tabs and bg image deprecated due to tabs image not working anymore
-    // do they exist? yes! But tabs will be out of normal usage for a long time... otherwise, the bg still works fine if enabled...
     public static RegistryObject<CreativeModeTab> ALL_ITEMS_TAB_v2 = CMTS.register("thingamajigs",
             () -> CreativeModeTab.builder()
                     .icon(() -> new ItemStack(ThingamajigsItems.THINGAMAJIG.get()))
                     .title(Component.translatable("itemGroup.Thingamajigs"))
                     //.withBackgroundLocation(new ResourceLocation("thingamajigs:textures/gui/thingamajigsitems.png"))
                     //.withTabsImage(new ResourceLocation("thingamajigs:textures/gui/thingamajigstabs.png"))
+                    .withTabsBefore(CreativeModeTabs.SPAWN_EGGS)
+                    .build());
+
+    public static RegistryObject<CreativeModeTab> ROAD_SIGNS_TAB = CMTS.register("thingamajigs_road_signs",
+            () -> CreativeModeTab.builder()
+                    .icon(() -> new ItemStack(ThingamajigsBlocks.STOP_SIGN.get()))
+                    .title(Component.translatable("itemGroup.ThingamajigsRoadSigns"))
+                    .withTabsBefore(ThingamajigsCreativeTab.ALL_ITEMS_TAB_v2.getKey())
+                    //.withBackgroundLocation(new ResourceLocation("thingamajigs:textures/gui/thingamajigsitems.png"))
                     .build());
 
     public static void register(IEventBus eventBus){
