33e5f74107090f5366cf6cd78fe9be45e45759cb
blueyMasterMods <roberttkahelin8@users.noreply.github.com>
2023-07-26 21:42:49 -0400

    Update ThingamajigsCreativeTab.java
    updated version to 1.20.1 ver

--- rawsrc/1.20/item/ThingamajigsCreativeTab.java
+++ rawsrc/1.20/item/ThingamajigsCreativeTab.java
@@ -2,6 +2,7 @@ package net.rk.thingamajigs.item;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
@@ -15,16 +16,17 @@ import net.rk.thingamajigs.block.ThingamajigsBlocks;
 @Mod.EventBusSubscriber(modid = Thingamajigs.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ThingamajigsCreativeTab {
     public static final DeferredRegister<CreativeModeTab> CMTS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Thingamajigs.MOD_ID);
+    public static final ResourceLocation ITEMS = new ResourceLocation("thingamajigs:textures/gui/thingamajigsitems.png");
+    public static final ResourceLocation ITEMS_LONGER = new ResourceLocation("thingamajigs:textures/gui/thingamajigssignitems.png");
+    public static final ResourceLocation TABS = new ResourceLocation("thingamajigs:textures/gui/thingamajigstabs.png");
 
-    // deprecated since mod ver 1.6.2-1.6.3+
-    // public static CreativeModeTab ALL_ITEMS_TAB;
 
     public static RegistryObject<CreativeModeTab> ALL_ITEMS_TAB_v2 = CMTS.register("thingamajigs",
             () -> CreativeModeTab.builder()
                     .icon(() -> new ItemStack(ThingamajigsItems.THINGAMAJIG.get()))
                     .title(Component.translatable("itemGroup.Thingamajigs"))
-                    //.withBackgroundLocation(new ResourceLocation("thingamajigs:textures/gui/thingamajigsitems.png"))
-                    //.withTabsImage(new ResourceLocation("thingamajigs:textures/gui/thingamajigstabs.png"))
+                    .withBackgroundLocation(ITEMS)
+                    .withTabsImage(TABS)
                     .withTabsBefore(CreativeModeTabs.SPAWN_EGGS)
                     .build());
 
@@ -33,7 +35,8 @@ public class ThingamajigsCreativeTab {
                     .icon(() -> new ItemStack(ThingamajigsBlocks.STOP_SIGN.get()))
                     .title(Component.translatable("itemGroup.ThingamajigsRoadSigns"))
                     .withTabsBefore(ThingamajigsCreativeTab.ALL_ITEMS_TAB_v2.getKey())
-                    //.withBackgroundLocation(new ResourceLocation("thingamajigs:textures/gui/thingamajigsitems.png"))
+                    .withBackgroundLocation(ITEMS_LONGER)
+                    .withTabsImage(TABS)
                     .build());
 
     public static void register(IEventBus eventBus){
